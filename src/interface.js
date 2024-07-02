6cff102af2e4267bc20c6c6177ee4289878f26ca
ivooo369 <ivoivelinov@abv.bg>
2024-07-02 21:10:33 +0300

    Fix a bug

--- src/interface.js
+++ src/interface.js
@@ -308,6 +308,12 @@ export function updateDataDisplay() {
       ? `${weatherData.currentHourlyTemperatureC} °C`
       : `${weatherData.currentHourlyTemperatureF} °F`;
   }
+
+  if (weatherData.isDay === 1) {
+    body.style.background = `url(${BackgroundDayImage})`;
+  } else {
+    body.style.background = `url(${BackgroundNightImage})`;
+  }
 }
 
 function updateUvIndex(uvIndex) {
